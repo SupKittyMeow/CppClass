b6d13d0b1416f580dc1d81838330b0bda45a207f
SupKittyMeow <77246128+SupKittyMeow@users.noreply.github.com>
2025-02-02 01:44:31 +0000

    not done yet

---
+++ .ccls-cache/@home@runner@workspace/icecreamthing.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> ingredients;
+string newIngredient;
+
+int main() {
+  while (true) {
+    cout << "What would you like to add to your ice cream?" << endl
+         << "Say 'done' when you are ready" << endl;
+
+    cin >> newIngredient;
+
+    if (newIngredient == "done") {
+      break;
+    }
+    ingredients.push_back(newIngredient);
+  }
+  return 0;
+}
